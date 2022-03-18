d49e84dec06751b95b3b4aa81cae1a0558638fef
Sangmin Kim <apfhdznzl@gmail.com>
2022-03-18 15:07:16 +0900

    feat: create NetworkCheckApi singleton class

---
+++ smnetworkchecker/src/main/java/com/sangmeebee/smnetworkchecker/NetworkCheckApi.kt
@@ -0,0 +1,64 @@
+package com.sangmeebee.smnetworkchecker
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import androidx.core.content.ContextCompat
+
+object NetworkCheckApi {
+
+    private var networkState = false
+
+    private var wifiState = false
+
+    private val networkRequest: NetworkRequest = NetworkRequest.Builder()
+        .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+        .build()
+
+    private val wifiRequest: NetworkRequest = NetworkRequest.Builder()
+        .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+        .build()
+
+    private val networkCallback = object : ConnectivityManager.NetworkCallback() {
+        // network is available for use
+        override fun onAvailable(network: Network) {
+            super.onAvailable(network)
+            networkState = true
+        }
+
+        // lost network connection
+        override fun onLost(network: Network) {
+            super.onLost(network)
+            networkState = false
+        }
+    }
+
+    private val wifiCallback = object : ConnectivityManager.NetworkCallback() {
+        // network is available for use
+        override fun onAvailable(network: Network) {
+            super.onAvailable(network)
+            wifiState = true
+
+        }
+
+        // lost network connection
+        override fun onLost(network: Network) {
+            super.onLost(network)
+            wifiState = false
+        }
+    }
+
+    // receive context and do request network
+    fun init(context: Context) {
+        val connectivityManager by lazy { ContextCompat.getSystemService(context, ConnectivityManager::class.java) as ConnectivityManager }
+
+        connectivityManager.requestNetwork(networkRequest, networkCallback)
+        connectivityManager.requestNetwork(wifiRequest, wifiCallback)
+
+    }
+
+    fun getNetWorkConnectState(): Boolean = networkState
+    fun getWifiState(): Boolean = wifiState
+}
